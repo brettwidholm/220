f16c1c79b7c5ba0817a295916b4a1bdd82425849
brettwidholm <widholmbm@g.cofc.edu>
2022-02-08 15:32:01 -0500

    Completed lab 4

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -20,11 +20,11 @@ def greeting_card():
     text = Text(Point(350, 50), "Happy Valentine's Day!!")
     text.draw(window)
 
-    arrow_bottom = Polygon(Point(0,325), Point(0, 375), Point(25,350))
+    arrow_bottom = Polygon(Point(0, 325), Point(0, 375), Point(25, 350))
     arrow_bottom.draw(window)
     arrow_body = Rectangle(Point(10, 355), Point(75, 345))
     arrow_body.draw(window)
-    arrow_head = Polygon(Point(75,325), Point(75, 375), Point(100,350))
+    arrow_head = Polygon(Point(75, 325), Point(75, 375), Point(100, 350))
     arrow_head.draw(window)
     arrow_bottom.setFill('brown')
     arrow_bottom.setOutline('brown')
@@ -33,8 +33,8 @@ def greeting_card():
     arrow_head.setFill('gray')
     arrow_head.setOutline('gray')
 
-    heart_bottom = Polygon(Point(350,500), Point(500, 300), Point(200,300))
-    heart_top_left = Circle(Point(425,250), 90)
+    heart_bottom = Polygon(Point(350, 500), Point(500, 300), Point(200, 300))
+    heart_top_left = Circle(Point(425, 250), 90)
     heart_top_right = Circle(Point(275, 250), 90)
 
     heart_bottom.setFill('pink')
@@ -52,9 +52,9 @@ def greeting_card():
 
     for i in range(100):
         time.sleep(0.1)
-        arrow_body.move(7,0)
-        arrow_head.move(7,0)
-        arrow_bottom.move(7,0)
+        arrow_body.move(7, 0)
+        arrow_head.move(7, 0)
+        arrow_bottom.move(7, 0)
 
     closer = Text(Point(350, 600), "Click anywhere to close")
     closer.draw(window)
