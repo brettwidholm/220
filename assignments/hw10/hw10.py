551f6929053bbe8178eddf6ce5b7ae99d64096fc
brettwidholm <97449224+brettwidholm@users.noreply.github.com>
2022-04-08 20:06:59 -0400

    Update hw10.py

--- assignments/hw10/hw10.py
+++ assignments/hw10/hw10.py
@@ -14,71 +14,75 @@ I certify that this assignment is entirely my own work.
 from graphics import GraphWin, Point
 from face import Face
 
-def fibonacci(n):
+def fibonacci(num):
     fibonacci_list = []
     count = 0
     initial_value = 1
     previous_value = 1
-    while count != n:
+    while count != num:
         fibonacci_list.append(initial_value)
         term = initial_value + previous_value
         initial_value = previous_value
         previous_value = term
         count += 1
-    return fibonacci_list[n-1]
+    return fibonacci_list[num-1]
 
 def double_investment(principle, rate):
     years = 0
     interest = principle
     while interest <= (2 * principle):
-        interest = principle * (1 + rate)
+        interest *= (1 + rate)
         years += 1
     return years
 
-def syracuse(n):
+def syracuse(num):
     syracuse_list = []
-    syracuse_list.append(n)
+    syracuse_list.append(num)
 
-    while n != 1:
-        if n % 2 == 0:
-            n = (n // 2)
-            syracuse_list.append(n)
-        elif n % 2 != 0:
-            n = (3 * n) + 1
-            syracuse_list.append(n)
+    while num != 1:
+        if num % 2 == 0:
+            num = (num // 2)
+            syracuse_list.append(num)
+        elif num % 2 != 0:
+            num = (3 * num) + 1
+            syracuse_list.append(num)
 
     return syracuse_list
 
-def goldbach(n):
+def goldbach(num):
     primes = []
     chosen_primes = []
     acc = 1
     count = 0
     i = 0
 
-
-    if n > 3:
+    if num > 3:
         primes.append(2)
         primes.append(3)
-    while acc <= (n / 6):
-        prime_number = (6 * acc) - 1
-        prime_number_2 = (6 * acc) + 1
-        primes.append(prime_number)
-        primes.append(prime_number_2)
-        acc += 1
+    while acc <= (num//6):
+        if acc <= 40:
+            prime_number = (6 * acc) - 1
+            prime_number_2 = (6 * acc) + 1
+            primes.append(prime_number)
+            primes.append(prime_number_2)
+            acc += 1
+        else:
+            prime_number_3 = (acc ** 2) + acc + 41
+            primes.append(prime_number_3)
+            acc += 1
 
-    if n % 2 != 0:
+    if num % 2 != 0:
         return None
-    elif n % 2 == 0:
+    elif num % 2 == 0:
         while count < (len(primes)):
-            if (n - primes[count]) != primes[i]:
+            if (num - primes[count]) != primes[i]:
                 i += 1
                 if i >= (len(primes)):
                     i = 0
                     count += 1
-            elif (n - primes[count]) == primes[i]:
-                chosen_primes.append((n - primes[count]))
-                chosen_primes.append(n - primes[i])
+            elif (num - primes[count]) == primes[i]:
+                chosen_primes.append((num - primes[count]))
+                chosen_primes.append(num - primes[i])
                 count = 1 + (len(primes))
         chosen_primes.sort()
         return chosen_primes
